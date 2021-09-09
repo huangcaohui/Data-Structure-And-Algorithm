d883693ce21f28d840719173fb29ded3e4202485
huangcaohui <1915589872@qq.com>
2021-09-09 15:07:09 +0800

    添加了内排序文件

---
+++ 内排序/插入排序.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+template <class Record>
+void ImprovedInsertSort(Record Array[], int n)  //Array[]为待排序数组，n为数组长度
+{
+    Record TempRecord;  //临时变量
+
+    for(int i = 0; i < n; i++)  //依次插入第i个记录
+    {
+        TempRecord = Array[i];
+
+        //从i开始往前寻找记录i的正确位置
+        int j = i-1;
+
+        //将那些大于等于记录i的记录后移
+
+        while(j > 0 && TempRecord < Array[j])
+        {
+            Array[j+1] = Array[j];
+            j = j - 1;
+        }
+
+        //此事j后面就是记录i的正确位置，回填
+        Array[j+1] = TempRecord;
+    }
+}
